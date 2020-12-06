b514d575dc1ed99dd7e2e1cb301b1cc157c5b611
ahmetilhann <ahmet.ddu@gmail.com>
2020-12-06 18:08:08 +0300

    version

--- core/src/main/java/academy/learnprogramming/Main.java
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -3,17 +3,16 @@ package academy.learnprogramming;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
-    private static final String CONFIG_LOCATION = "beans.xml";
-
     public static void main(String[] args) {
         log.info("Guess The Game");
 
-        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
         NumberGenerator numberGenerator = context.getBean(NumberGenerator.class);
 
@@ -21,7 +20,9 @@ public class Main {
 
         log.info("Number: {}", number);
 
-        Game game = context.getBean(Game.class);
+        MessageGenerator messageGenerator = context.getBean(MessageGenerator.class);
+        log.info("getMainMessage = {}", messageGenerator.getMainMessage());
+        log.info("getResultMessage = {}", messageGenerator.getResultMessage());
 
         context.close();
     }
